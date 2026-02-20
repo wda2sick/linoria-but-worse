9bad981dae48f445319e9dad3ac5813f9c8b639c
wda2sick <kris.petrovw2012@gmail.com>
2026-02-20 17:59:31 +0200

    Change BackgroundColor3 to BackgroundTransparency

--- Library.lua
+++ Library.lua
@@ -2869,7 +2869,7 @@ function Library:CreateWindow(...)
 
     local Outer = Library:Create('Frame', {
         AnchorPoint = Config.AnchorPoint,
-        BackgroundColor3 = Color3.new(0, 0, 0);
+        BackgroundTransparency = 1;
         BorderSizePixel = 0;
         Position = Config.Position,
         Size = Config.Size,
